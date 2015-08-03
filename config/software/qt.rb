397c578322ad9977072aa0d3434de0d87eb36d33
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-03 15:07:17 -0400

    FIXME: get rid of qt

--- config/software/qt.rb
+++ config/software/qt.rb
@@ -2,9 +2,16 @@ name "qt"
 
 default_version "4.8"
 
-dependency "homebrew"
+if ohai['platform'] == 'mac_os_x'
+  dependency "homebrew"
 
-build do
-  command "brew install qt"
-  command "brew linkapps qt"
+  build do
+    command "brew install qt"
+    command "brew linkapps qt"
+  end
+elsif ohai['platform'] == 'mac_os_x'
+  # We create a dummy file for the omnibus git_cache to work on Windows
+  build do
+    command "touch #{install_dir}/uselessfile"
+  end
 end
