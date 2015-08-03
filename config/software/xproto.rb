22eed4b9cac05e6c5a1616a322f6e1dba541474c
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-03 13:14:47 -0400

    Don't build xproto on windows

--- config/software/xproto.rb
+++ config/software/xproto.rb
@@ -2,46 +2,55 @@
 name "xproto"
 default_version "7.0.27"
 
-source :url => "http://xorg.freedesktop.org/releases/individual/proto/xproto-#{version}.tar.gz",
-  :md5 => 'f04f535b090f3fd05073370740e99193'
+if ohai['platform'] != 'windows'
 
-relative_path "xproto-#{version}"
+  source :url => "http://xorg.freedesktop.org/releases/individual/proto/xproto-#{version}.tar.gz",
+    :md5 => 'f04f535b090f3fd05073370740e99193'
 
-configure_env =
-  case ohai['platform']
-  when "aix"
-    {
-      "CC" => "xlc -q64",
-      "CXX" => "xlC -q64",
-      "LD" => "ld -b64",
-      "CFLAGS" => "-q64 -I#{install_dir}/embedded/include -O",
-      "LDFLAGS" => "-q64 -Wl,-blibpath:/usr/lib:/lib",
-      "OBJECT_MODE" => "64",
-      "ARFLAGS" => "-X64 cru",
-      "LD" => "ld -b64",
-      "OBJECT_MODE" => "64",
-      "ARFLAGS" => "-X64 cru "
-    }
-  when "mac_os_x"
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-I#{install_dir}/embedded/include -L#{install_dir}/embedded/lib"
-    }
-  when "solaris2"
-    {
-      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
-    }
-  else
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-I#{install_dir}/embedded/include -L#{install_dir}/embedded/lib"
-    }
+  relative_path "xproto-#{version}"
+
+  configure_env =
+    case ohai['platform']
+    when "aix"
+      {
+        "CC" => "xlc -q64",
+        "CXX" => "xlC -q64",
+        "LD" => "ld -b64",
+        "CFLAGS" => "-q64 -I#{install_dir}/embedded/include -O",
+        "LDFLAGS" => "-q64 -Wl,-blibpath:/usr/lib:/lib",
+        "OBJECT_MODE" => "64",
+        "ARFLAGS" => "-X64 cru",
+        "LD" => "ld -b64",
+        "OBJECT_MODE" => "64",
+        "ARFLAGS" => "-X64 cru "
+      }
+    when "mac_os_x"
+      {
+        "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+        "CFLAGS" => "-I#{install_dir}/embedded/include -L#{install_dir}/embedded/lib"
+      }
+    when "solaris2"
+      {
+        "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+        "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
+      }
+    else
+      {
+        "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+        "CFLAGS" => "-I#{install_dir}/embedded/include -L#{install_dir}/embedded/lib"
+      }
+    end
+
+  build do
+    ship_license "http://cgit.freedesktop.org/xorg/proto/xproto/plain/COPYING"
+    command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
+    command "make -j #{workers}", :env => configure_env
+    command "make -j #{workers} install", :env => configure_env
   end
 
-build do
-  ship_license "http://cgit.freedesktop.org/xorg/proto/xproto/plain/COPYING"
-  command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
-  command "make -j #{workers}", :env => configure_env
-  command "make -j #{workers} install", :env => configure_env
+else
+  # We create a dummy file for the omnibus git_cache to work on Windows
+  build do
+    command "touch #{install_dir}/uselessfile"
+  end
 end
