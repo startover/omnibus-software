0e9b6e1c38e884137252c8b40cde949a1d085059
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-05 19:22:26 -0400

    big regression on my compiling ambitions

--- config/software/libpq.rb
+++ config/software/libpq.rb
@@ -1,10 +1,11 @@
 name "libpq"
 default_version "9.4.0"
 
-source :url => "http://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.gz",
-       :md5 => "349552802c809c4e8b09d8045a437787"
 
 if ohai['platform'] != 'windows'
+
+  source :url => "http://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.gz",
+         :md5 => "349552802c809c4e8b09d8045a437787"
 
   dependency "zlib"
   dependency "openssl"
@@ -36,33 +37,15 @@ if ohai['platform'] != 'windows'
   end
 
 else
-  # We create a dummy file for the omnibus git_cache to work on Windows
+  source :url => "http://get.enterprisedb.com/postgresql/postgresql-9.4.4-3-windows-x64-binaries.zip",
+         :md5 => ""
+
   build do
     ship_license "https://raw.githubusercontent.com/lpsmith/postgresql-libpq/master/LICENSE"
 
-    env = {
-      "LDFLAGS" => "-L#{windows_safe_path(install_dir)}\\embedded\\lib -I#{windows_safe_path(install_dir)}\\embedded\\include",
-      "CFLAGS" => "-L#{windows_safe_path(install_dir)}\\embedded\\lib -I#{windows_safe_path(install_dir)}\\embedded\\include",
-      "LD_RUN_PATH" => "#{windows_safe_path(install_dir)}\\embedded\\lib"
-    }
-
-    # make is actually shipped with chefdk... Pretty convenient isn't it ? As long as we build in a
-    # chef provision machine we have it in our path, which is awesome. In case we want to change
-    # at some point, we'll have to add a software definition that downloads and installs MinGW or
-    # Cygwin to compile pg_config and libpq.
-    command "pwd", :env => env
-    command "C:\\opscode\\chefdk\\embedded\\bash.exe -c 'ls -la .'"
-    command "bash -c 'pwd'"
-    command [ "bash -c './configure",
-              "--prefix=#{install_dir}//embedded",
-              "--with-libedit-preferred",
-              "--with-openssl",
-              "--with-includes=#{install_dir}/embedded/include",
-              "--with-libraries=#{install_dir}/embedded/lib '" ].join(" "), :env => env
-    command "make -j #{workers}", :env => { "LD_RUN_PATH" => "#{windows_safe_path(install_dir)}\\embedded\\lib"}
+    # After trying to compile unsuccessfully with VisualC++ and MinGW let's stick to the
+    # standard windows "solutions" : download the binaries and making a nice copy of it :)
     mkdir "#{windows_safe_path(install_dir)}\\embedded\\include\\postgresql"
-    command "make -C src\\include install"
-    command "make -C src\\interfaces install"
-    command "make -C src\\bin\\pg_config install"
+    command "COPY bin\pg_config.exe #{windows_safe_path(install_dir)}\\embedded\\bin"
   end
 end
