177352df0d7dfc0895f4042fb8b9e96571b5830b
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-05 19:46:01 -0400

    And now ?

--- config/software/libpq.rb
+++ config/software/libpq.rb
@@ -37,15 +37,5 @@ if ohai['platform'] != 'windows'
   end
 
 else
-  source :url => "http://get.enterprisedb.com/postgresql/postgresql-9.4.4-3-windows-x64-binaries.zip",
-         :md5 => "094d18f3534d1a6c1cab17d37fb88319"
-
-  build do
-    ship_license "https://raw.githubusercontent.com/lpsmith/postgresql-libpq/master/LICENSE"
-
-    # After trying to compile unsuccessfully with VisualC++ and MinGW let's stick to the
-    # standard windows "solutions" : download the binaries and making a nice copy of it :)
-    mkdir "#{windows_safe_path(install_dir)}\\embedded\\include\\postgresql"
-    command "COPY bin\pg_config.exe #{windows_safe_path(install_dir)}\\embedded\\bin"
-  end
+  dependency 'pgsql'
 end
