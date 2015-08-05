73fabc6acbaecdcdc53b3f5090b29be1a01429fd
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-05 14:09:28 -0400

    PyOpenSSL shipped on windose

--- config/software/pyopenssl.rb
+++ config/software/pyopenssl.rb
@@ -3,20 +3,23 @@ default_version "0.14"
 
 if ohai['platform'] == 'windows'
   dependency "openssl-windows"
+  dependency "python"
+  dependency "pip"
+  command "#{install_dir}/embedded/bin/pip install -I pyopenssl==#{version}"
 else
   dependency "openssl"
-end
-dependency "python"
-dependency "pip"
-dependency "libffi"
+  dependency "libffi"
+  dependency "python"
+  dependency "pip"
 
-build do
-  ship_license "https://raw.githubusercontent.com/pyca/pyopenssl/master/LICENSE"
-  build_env = {
-    "PATH" => "/#{install_dir}/embedded/bin:#{ENV['PATH']}",
-    "LDFLAGS" => "-L/#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include/"
-  }
-  command "#{install_dir}/embedded/bin/pip install -I pyOpenSSL==#{version}", :env => build_env
+  build do
+    ship_license "https://raw.githubusercontent.com/pyca/pyopenssl/master/LICENSE"
+    build_env = {
+      "PATH" => "/#{install_dir}/embedded/bin:#{ENV['PATH']}",
+      "LDFLAGS" => "-L/#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include/"
+    }
+    command "#{install_dir}/embedded/bin/pip install -I pyOpenSSL==#{version}", :env => build_env
+  end
 end
