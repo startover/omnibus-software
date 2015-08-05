1b72909f5442aaa9b95a15423ad21d640cd8468a
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-05 15:45:02 -0400

    Forgotten

--- config/software/vc_python.rb
+++ config/software/vc_python.rb
@@ -3,7 +3,7 @@
 name 'vc_python'
 default_version '2.7'
 
-source 'http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+source :url => 'http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
 
 build do
     command "start /wait msiexec /x VCForPython27.msi /qn"
