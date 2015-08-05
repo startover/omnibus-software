3168caf4d479a023906276a6e63be12e9b1d5157
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-05 16:01:59 -0400

    Add VC++ checksum

--- config/software/vc_python.rb
+++ config/software/vc_python.rb
@@ -3,7 +3,8 @@
 name 'vc_python'
 default_version '2.7'
 
-source :url => 'http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+source :url => 'http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi',
+       :md5 => '4e6342923a8153a94d44ff7307fcdd1f'
 
 build do
     command "start /wait msiexec /x VCForPython27.msi /qn"
