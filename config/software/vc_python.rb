f4de11782124033872a2d7406859b3ee04c6183b
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-05 15:43:27 -0400

    PyCrypto compiles on Windows
    And now we have Visual C++ for Python s a dependency so mybe we can try
    compiling other pieces of stuff on Windows :)

---
+++ config/software/vc_python.rb
@@ -0,0 +1,12 @@
+# Microsoft Visual C++ for Python
+
+name 'vc_python'
+default_version '2.7'
+
+source 'http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+
+build do
+    command "start /wait msiexec /x VCForPython27.msi /qn"
+    command "start /wait msiexec /i VCForPython27.msi /qn"
+end
+
