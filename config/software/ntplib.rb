ec7afce02f49543349306fa94fe3b0bca445ce90
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-06 12:19:56 -0400

    Grr

--- config/software/ntplib.rb
+++ config/software/ntplib.rb
@@ -6,5 +6,9 @@ dependency "pip"
 
 build do
   ship_license "MIT"
-  command "#{install_dir}/embedded/bin/pip install --force-reinstall -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}", :cwd => "/tmp"
+  if ohai['platform'] == 'windows'
+    command "#{install_dir}/embedded/Scripts/pip install --force-reinstall -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}", :cwd => "/tmp"
+  else
+    command "#{install_dir}/embedded/bin/pip install --force-reinstall -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}", :cwd => "/tmp"
+  end
 end
