d33a679485d32f7bde80fe86b96d56a50cb37518
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-06 12:07:41 -0400

    Verdicto

--- config/software/pysnmp-mibs.rb
+++ config/software/pysnmp-mibs.rb
@@ -8,9 +8,8 @@ dependency "setuptools"
 build do
   ship_license "https://gist.githubusercontent.com/remh/519324dc1b69f7488239/raw/2bbf2888194fef8ae75651e551b61f90cb49c482/pysnmp.license"
   if ohai['platform'] == 'windows'
-    command "#{windows_safe_path(install_dir)}\\embedded\\bin\\pip install -I --install-option="\
+    command "#{windows_safe_path(install_dir)}\\embedded\\Scripts\\pip install -I --install-option="\
             "\"--install-scripts=#{windows_safe_path(install_dir)}\\bin\" "\
-            "--prefix=\"#{windows_safe_path(install_dir)}\\embedded\" "\
             "#{name}==#{version}"
   else
     command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
