b48805352453ae68e7062cee47bbed130c49e5aa
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-06 12:29:32 -0400

    again

--- config/software/simplejson.rb
+++ config/software/simplejson.rb
@@ -6,5 +6,9 @@ dependency "pip"
 
 build do
   ship_license "https://raw.githubusercontent.com/simplejson/simplejson/master/LICENSE.txt"
-  command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  if ohai['platform'] == 'windows'
+    command "#{install_dir}/embedded/Scripts/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  else
+    command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  end
 end
