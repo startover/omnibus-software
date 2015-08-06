5de6af3cc98245f16a5d95659cd1bd880379852c
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-06 12:02:57 -0400

    Gnn

--- config/software/futures.rb
+++ config/software/futures.rb
@@ -7,7 +7,7 @@ dependency "pip"
 build do
   ship_license "https://pythonfutures.googlecode.com/hg/LICENSE"
   if ohai['platform'] == 'windows'
-    command "#{windows_safe_path(install_dir)}\\embedded\\pip install -I --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}\\bin\" #{name}==#{version}"
+    command "#{windows_safe_path(install_dir)}\\embedded\\Scripts\\pip install -I --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}\\bin\" #{name}==#{version}"
   else
     command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
   end
