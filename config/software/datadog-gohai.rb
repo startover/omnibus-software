45df07f001dcf06a3b30b574615263bb5bc7b289
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-06 16:49:25 -0400

    Removed unnecessary line

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -12,7 +12,6 @@ if ohai['platform'] == 'windows'
 
   build do
     command "cmd /C \"SET GOPATH=\"#{Omnibus::Config.cache_dir}\\src\\#{name}\" && "\
-            "SET GOROOT=\"C:\\golang_omnibus\\\" && "\
             "#{gobin} get -d -u github.com/DataDog/gohai && "\
             "CHDIR #{Omnibus::Config.cache_dir}\\src\\#{name}\\src\\github.com\\Datadog\\gohai && "\
             "git checkout #{version} && git pull && "\
