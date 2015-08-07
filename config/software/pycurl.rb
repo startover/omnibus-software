fed77bb4b8e9cd8b603ff50591d98b981f0248de
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-07 18:25:20 -0400

    Curly

--- config/software/pycurl.rb
+++ config/software/pycurl.rb
@@ -1,9 +1,10 @@
 name "pycurl"
 default_version "7.19.5.1"
 
+dependency "python"
+dependency "pip"
+
 if ohai['platform'] != "windows"
-  dependency "python"
-  dependency "pip"
   dependency "curl"
   dependency "gdbm" if (ohai['platform'] == "mac_os_x" or ohai['platform'] == "freebsd" or ohai['platform'] == "aix")
   dependency "libgcc" if (ohai['platform'] == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
@@ -12,13 +13,13 @@ if ohai['platform'] != "windows"
     ship_license "https://raw.githubusercontent.com/pycurl/pycurl/master/COPYING-MIT"
     build_env = {
       "PATH" => "/#{install_dir}/embedded/bin:#{ENV['PATH']}",
+      # FIXME : eeeehm, shouldn't it rather be depending on the architecture of the build platform
       "ARCHFLAGS" => "-arch x86_64"
     }
     command "#{install_dir}/embedded/bin/pip install -I #{name}==#{version}", :env => build_env
   end
 else
-  # FIXME: we could actually try to compile PyCurl even on Windows
   build do
-    command "touch #{windows_safe_path(install_dir)}\\uselessfile"
+    command "#{install_dir}/embedded/Scripts/pip install -I #{name}==#{version}"
   end
 end
