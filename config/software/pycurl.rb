d5b1cad634e9e8c47d814e482d37cb60e127ce1e
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-12 18:39:27 -0400

    Pimp my wheel

--- config/software/pycurl.rb
+++ config/software/pycurl.rb
@@ -3,9 +3,9 @@ default_version "7.19.5.1"
 
 dependency "python"
 dependency "pip"
-dependency "curl"
 
 if ohai['platform'] != "windows"
+  dependency "curl"
   dependency "gdbm" if (ohai['platform'] == "mac_os_x" or ohai['platform'] == "freebsd" or ohai['platform'] == "aix")
   dependency "libgcc" if (ohai['platform'] == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
 
@@ -20,13 +20,13 @@ if ohai['platform'] != "windows"
   end
 else
   build do
-    source :url => "http://pycurl.sourceforge.net/download/pycurl-#{version}.tar.gz",
-           :md5 => 'f44cd54256d7a643ab7b16e3f409b26b'
+    source :url => "http://www.lfd.uci.edu/~gohlke/pythonlibs/3i673h27/pycurl-7.19.5.1-cp35-none-win_amd64.whl",
+           :md5 => ''
 
     relative_path "pycurl-#{version}"
 
-    command "#{install_dir}/embedded/python setup.py --use-libcurl-dll --curl-dir="\
-            "#{install_dir}/embedded/ --libcurl-lib-name=cygcurl.dll --avoid-stdio"
+    # command "#{install_dir}/embedded/python setup.py --use-libcurl-dll --curl-dir="\
+    #         "#{install_dir}/embedded/ --libcurl-lib-name=cygcurl.dll --avoid-stdio"
     # command "SET PYCURL_SETUP_OPTIONS=\"--avoid-stdio --use-libcurl-dll --curl-dir="\
     #         "#{install_dir}/embedded/Lib --libcurl-lib-name=cygcurl.dll\" & "\
     #         "#{install_dir}/embedded/Scripts/pip install -I #{name}==#{version}"
